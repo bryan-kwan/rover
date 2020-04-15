aba58e7238a8667687d1c676c6eb8c4058b854cf
bryan-kwan <59986136+bryan-kwan@users.noreply.github.com>
2020-04-15 12:31:15 -0600

    Update move.py

--- move.py
+++ move.py
@@ -4,20 +4,20 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
 
 
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
-
-
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 
 
 
-GPIO.output(17, GPIO.HIGH)
+
+
+GPIO.output(11, GPIO.HIGH)
 sleep(2)
-GPIO.output(17, GPIO.LOW)
+GPIO.output(11, GPIO.LOW)
 
 GPIO.cleanup()
 print("Exiting")
